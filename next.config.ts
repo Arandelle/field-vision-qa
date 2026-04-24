1f8cdd9dab1a6861fd94f65c72a04154dd3eeaac
Arandelle <topaguintsarandell@gmail.com>
2026-04-24 17:30:27 +0800

    chore: allow cloudflare tunnel origin for local dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   allowedDevOrigins: ['amendment-ranks-gold-config.trycloudflare.com'],
 };
 
 export default nextConfig;
